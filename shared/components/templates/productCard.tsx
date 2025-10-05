511f5064ab4f75d8db3026cbccdb7efbb825b981
PriyathamVarma <priyatham002@gmail.com>
2025-10-05 14:45:54 +0530

    (fix):currency symbol ₹

--- shared/components/templates/productCard.tsx
+++ shared/components/templates/productCard.tsx
@@ -49,7 +49,7 @@ export default function ProductCard({
           </div>
 
           <div className="text-right flex-shrink-0">
-            <div className="text-lg font-bold text-stone-800">${product.price.toFixed(2)}</div>
+            <div className="text-lg font-bold text-stone-800">₹{product.price.toFixed(2)}</div>
             {product.badge && (
               <div className="text-xs mt-2 inline-block bg-yellow-100 text-yellow-800 px-2 py-1 rounded-full">
                 {product.badge}
